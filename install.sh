c6e322ad82c8e4457157a84af6539947f6f35adc
Corbin Davenport <me@corbin.io>
2018-04-30 22:55:43 -0400

    Platform tools now downloaded directly from Google

--- install.sh
+++ install.sh
@@ -13,50 +13,24 @@
 
 #!/bin/bash
 
-ADB="$HOME/.nexustools/adb"
-FASTBOOT="$HOME/.nexustools/fastboot"
+DIR="$HOME/.nexustools"
 UDEV="/etc/udev/rules.d/51-android.rules"
 OS=$(uname)
 ARCH=$(uname -m)
 XCODE=0
 BASEURL="https://github.com/corbindavenport/nexus-tools/raw/master"
 
-_install() {
-	sudo curl -Lfks -o "$1" "$2" && echo "[ OK ] Download succeeded."|| { echo "[EROR] Download failed."; XCODE=1; }
-}
-
-# If Nexus Tools is detects the ADB and/or Fastboot binaries, and it cannot detect it as part of an Linux package it can remove, it removes the binaries themselves. This is the default for macOS.
-_dirty_remove() {
-	if [ -f "$HOME/.nexustools/nexus-tools.txt" ]; then
-		echo "[ OK ] Log file detected."
-	fi
-	if [ -f $ADB ]; then
-			echo "[WARN] ADB is already present, press ENTER to overwrite or X to cancel."
-			read -sn1 input
-			[ "$input" = "" ] && sudo rm $ADB || exit 1
-	fi
-	if [ -f $FASTBOOT ]; then
-			echo "[WARN] Fastboot is already present, press ENTER to overwrite or X to cancel."
-			read -sn1 input
-			[ "$input" = "" ] && sudo rm $FASTBOOT || exit 1
-	fi
-}
-
 # Nexus Tools can check if a package for ADB or Fastboot is installed, and uninstall the package if needed.
 _smart_remove() {
 	if [ -x "$(command -v dpkg)" ]; then # Linux systems with dpkg
 		PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $1|grep "install ok installed")
 		if [ "" == "$PKG_OK" ]; then # Check if relevant package is installed
-				echo "[ OK] The package '$1' is not installed, install can continue."
-				_dirty_remove
+			echo "[ OK ] The package '$1' is not installed, install can continue."
 		else
 			echo "[WARN] An outdated version of ADB or Fastboot is already installed, as part of the '$1' system package. Press ENTER to remove it or X to cancel."
 			read -sn1 input
 			[ "$input" = "" ] && sudo apt-get --assume-yes remove $1 && echo "[ OK ] The '$1' package was removed." || exit 1
 		fi
-	else
-		echo "[INFO] Linux is detected but dpkg is either not working or not installing, now attempting dirty removal."
-		_dirty_remove
 	fi
 }
 
@@ -67,57 +41,58 @@ _install_udev() {
 			sudo mkdir -p /etc/udev/rules.d/
 		fi
 
-	local install=1
+		local install=1
 
-	if [ -f "$UDEV" ]; then
-		sudo rm "$UDEV"
-		echo "[ OK ] Udev rules are being overwritten."
-	fi
+		if [ -f "$UDEV" ]; then
+			sudo rm "$UDEV"
+			echo "[ OK ] Udev rules are being overwritten."
+		fi
 
-	if [ $install -eq 1 ]; then
+		if [ $install -eq 1 ]; then
 
-		echo "[INFO] Downloading udev list..."
-		_install "$UDEV" "$BASEURL/udev.txt"
+			echo "[INFO] Downloading udev list..."
+			sudo curl -Lfks -o "$UDEV" "$BASEURL/udev.txt"
 
-		output=$(sudo chmod 644 $UDEV 2>&1) && echo "[ OK ] UDEV permissions fixed." || { echo "[EROR] $output"; XCODE=1; }
+			output=$(sudo chmod 644 $UDEV 2>&1) && echo "[ OK ] UDEV permissions fixed." || { echo "[EROR] $output"; XCODE=1; }
 
-		output=$(sudo chown root: $UDEV 2>&1) && echo "[ OK ] UDEV ownership fixed." || { echo "[EROR] $output"; XCODE=1; }
+			output=$(sudo chown root: $UDEV 2>&1) && echo "[ OK ] UDEV ownership fixed." || { echo "[EROR] $output"; XCODE=1; }
 
-		sudo service udev restart 2>/dev/null >&2
-		sudo killall adb 2>/dev/null >&2
-	else
-		echo "[INFO] Skipping UDEV..."
-	fi
+			sudo service udev restart 2>/dev/null >&2
+			sudo killall adb 2>/dev/null >&2
+		else
+			echo "[INFO] Skipping UDEV..."
+		fi
+
 	fi
 }
 
 # Function for adding Nexus Tools directory to $PATH
 _add_path() {
 	if [ "$OS" == "Darwin" ]; then # macOS
-		if [[ ":$PATH:" == *":$HOME/.nexustools:"* ]]; then
+		if [[ ":$PATH:" == *":$DIR:"* ]]; then
 			# Nexus Tools directory already in $PATH
-			echo "[ OK ] $HOME/.nexustools/ is already in PATH."
+			echo "[ OK ] $DIR/ is already in PATH."
 		else
 			# Nexus Tools directory needs to be added to $PATH
 			echo 'export PATH=$PATH:~/.nexustools' >> ~/.bash_profile
 			# Refresh path
 			source $HOME/.bash_profile
-			echo "[ OK ] Added $HOME/.nexustools/ to PATH."
+			echo "[ OK ] Added $DIR/ to PATH."
 		fi
 	elif [ "$OS" == "Linux" ]; then # Generic Linux
-		if [[ ":$PATH:" == *":$HOME/.nexustools:"* ]]; then
+		if [[ ":$PATH:" == *":$DIR:"* ]]; then
 			# Nexus Tools directory already in $PATH
-			echo "[ OK ] $HOME/.nexustools/ is already in PATH."
+			echo "[ OK ] $DIR/ is already in PATH."
 		else
 			# Nexus Tools directory needs to be added to $PATH
-			PATH="$PATH:$HOME/.nexustools"
-			echo "[ OK ] Added $HOME/.nexustools/ to PATH."
+			PATH="$PATH:$DIR"
+			echo "[ OK ] Added $DIR/ to PATH."
 		fi
 	fi
 }
 
 # Get sudo
-echo "[INFO] Nexus Tools 3.2"
+echo "[INFO] Nexus Tools 4.0"
 if [ "$OS" == "Linux" ]; then
 	DIST=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
 	if [ -z "${DIST##*Ubuntu*}" ] || [ -z "${DIST##*Debian*}" ]; then
@@ -129,7 +104,17 @@ fi
 echo "[INFO] Please enter sudo password for install."
 sudo echo "[ OK ] Sudo access granted." || { echo "[ERROR] No sudo access."; exit 1; }
 
-# Check if ADB or Fastboot is already on this computer
+# Delete existing Nexus Tools installation if it exists
+if [ -d $DIR ]; then
+	echo "[WARN] Platform tools already installed in $DIR. press ENTER to overwrite or X to cancel."
+	read -sn1 input
+	[ "$input" = "" ] && rm -rf $DIR || exit 1
+fi
+
+# Make the new directory
+mkdir -p $DIR
+
+# Check if ADB or Fastboot is already installed
 if [ "$OS" == "Linux" ]; then
 	DIST=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
 	# If someone wants to add support, this should work with any distro using dpkg for package management. Just change the paramteter to whatever package installs ADB/Fastboot binaries.
@@ -140,49 +125,51 @@ if [ "$OS" == "Linux" ]; then
 		_smart_remove "android-tools-adb"
 		_smart_remove "android-tools-fastboot"
 	else
-		echo "[WARN] Nexus Tools cannot detect if you have an ADB/Fastboot package already installed on your system."
-		echo "[WARN] Nexus Tools will still check for ADB/Fastboot binaries, but if they are installed as a package it may overwrite the original files."
-		_dirty_remove
+		# For other distros, check if either adb or fastboot is installed using command
+		command -v adb >/dev/null 2>&1 || { echo "[EROR] ADB is already installed and Nexus Tools cannot remove it automatically. Please manually uninstall ADB and try again." >&2; exit 1; }
+		command -v fastboot >/dev/null 2>&1 || { echo "[EROR] Fastboot is already installed and Nexus Tools cannot remove it automatically. Please manually uninstall Fastboot and try again." >&2; exit 1; }
 	fi
-else
-	_dirty_remove
+elif [ "$OS" == "Darwin" ]; then # macOS
+	# For macOS, check if either adb or fastboot is installed using command
+	command -v adb >/dev/null 2>&1 || { echo "[EROR] ADB is already installed and Nexus Tools cannot remove it automatically. Please manually uninstall ADB and try again." >&2; exit 1; }
+	command -v fastboot >/dev/null 2>&1 || { echo "[EROR] Fastboot is already installed and Nexus Tools cannot remove it automatically. Please manually uninstall Fastboot and try again." >&2; exit 1; }
 fi
-
-# Check if bin folder is already created
-mkdir -p $HOME/.nexustools
 
 # Detect operating system and install
 if [ -d "/mnt/c/Windows" ]; then # Windows 10 Bash
-	echo "[EROR] Bash on Windows 10 does not yet support UDEV, which is required for ADB and Fastboot to work."
+	echo "[EROR] Bash on Windows 10 does not yet support USB devices."
 	exit $XCODE
 elif [ "$OS" == "Darwin" ]; then # macOS
-	echo "[INFO] Downloading ADB for macOS..."
-	_install "$ADB" "$BASEURL/bin/mac-adb"
-	echo "[INFO] Downloading Fastboot for macOS..."
-	_install "$FASTBOOT" "$BASEURL/bin/mac-fastboot"
-	# Skip udev install because macOS doesn't use it
-	echo "[INFO] Making ADB and Fastboot executable..."
-	output=$(sudo chmod +x $ADB 2>&1) && echo "[INFO] ADB now executable." || { echo "[EROR] $output"; XCODE=1; }
-	output=$(sudo chmod +x $FASTBOOT 2>&1) && echo "[INFO] Fastboot now executable." || { echo "[EROR] $output"; XCODE=1; }
-	# Add Nexus Tools directory to path
-	_add_path
-	# All done!
-	[ $XCODE -eq 0 ] && { echo "[ OK ] Done!"; echo "[INFO] Type adb or fastboot to run, you may need to open a new Terminal window for it to work."; echo "[INFO] If you found Nexus Tools helpful, please consider donating to support development: bit.ly/donatenexustools"; } || { echo "[EROR] Install failed"; }
-	echo " "
-	exit $XCODE
-
+	ZIP="https://dl.google.com/android/repository/platform-tools-latest-darwin.zip"
+	# Download the ZIP file
+	echo "[ .. ] Downloading platform tools for x86 Linux..."
+	curl -Lfks -o "$DIR/temp.zip" "$ZIP" && echo "[ OK ] Download succeeded."|| { echo "[EROR] Download failed."; XCODE=1; }
+	# Unzip it
+	unzip -q -o "$DIR/temp.zip" -d "$DIR"
+	# Move all files from the zip to $DIR
+	mv -f -v $DIR/platform-tools/* $DIR > /dev/null
+	# Delete the zip file and original folder
+	rm "$DIR/temp.zip"
+	rmdir "$DIR/platform-tools"
+	echo "[ OK ] Platform tools now installed in $DIR."
 elif [ "$OS" == "Linux" ]; then # Generic Linux
 	if [ "$ARCH" == "i386" ] || [ "$ARCH" == "i486" ] || [ "$ARCH" == "i586" ] || [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "i686" ]; then # Linux on Intel x86/x86_64 CPU
-		echo "[INFO] Downloading ADB for Linux [Intel CPU]..."
-		_install "$ADB" "$BASEURL/bin/linux-i386-adb"
-		echo "[INFO] Downloading Fastboot for Linux [Intel CPU]..."
-		_install "$FASTBOOT" "$BASEURL/bin/linux-i386-fastboot"
+		ZIP="https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
+		# Download the ZIP file
+		echo "[ .. ] Downloading platform tools for x86 Linux..."
+		curl -Lfks -o "$DIR/temp.zip" "$ZIP" && echo "[ OK ] Download succeeded."|| { echo "[EROR] Download failed."; XCODE=1; }
+		# Unzip it
+		unzip -q -o "$DIR/temp.zip" -d "$DIR"
+		# Move all files from the zip to $DIR
+		mv -f -v $DIR/platform-tools/* $DIR > /dev/null
+		# Delete the zip file and original folder
+		rm "$DIR/temp.zip"
+		rmdir "$DIR/platform-tools"
+		echo "[ OK ] Platform tools now installed in $DIR."
 	elif [ "$ARCH" == "arm" ] || [ "$ARCH" == "armv6l" ] || [ "$ARCH" == "armv7l" ]; then # Linux on ARM CPU
-		echo "[WARN] The ADB binaries for ARM are out of date, and do not work with Android 4.2.2 and higher"
-		echo "[INFO] Downloading ADB for Linux [ARM CPU]..."
-		_install "$ADB" "$BASEURL/bin/linux-arm-adb"
-		echo "[INFO] Downloading Fastboot for Linux [ARM CPU]..."
-		_install "$FASTBOOT" "$BASEURL/bin/linux-arm-fastboot"
+		echo "[EROR] Your platform does not have up-to-date binaries available. Cannot continue with installation."
+		echo " "
+		exit 1
 	else
 		echo "[EROR] Your CPU architecture could not be detected."
 		echo "[EROR] Report bugs at: github.com/corbindavenport/nexus-tools/issues"
@@ -194,8 +181,8 @@ elif [ "$OS" == "Linux" ]; then # Generic Linux
 	fi
 	# Download udev list
 	_install_udev
-	output=$(sudo chmod +x $ADB 2>&1) && echo "[ OK ] Marked ADB as executable." || { echo "[EROR] $output"; XCODE=1; }
-	output=$(sudo chmod +x $FASTBOOT 2>&1) && echo "[ OK ] Marked Fastboot as executable." || { echo "[EROR] $output"; XCODE=1; }
+	# Mark binaries in directory as executable
+	chmod -f +x $DIR/*
 	# Add Nexus Tools directory to path
 	_add_path
 	# All done!
