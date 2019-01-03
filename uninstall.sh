31fa5a6fcd3558d83645f3ef8f27e25a5a2b48d8
Corbin Davenport <me@corbin.io>
2019-01-03 01:32:16 -0500

    Fix formatting, add more console messages

--- uninstall.sh
+++ uninstall.sh
@@ -18,39 +18,44 @@ ARCH=$(uname -m)
 
 echo "[INFO] Nexus Tools 4.0"
 
-# Nexus Tools 3.2+ (August 2016-) installs binaries in ~/.nexustools
+# Nexus Tools 3.2+ (August 2016-Now) installs binaries in ~/.nexustools
 if [ -d $HOME/.nexustools ]; then
 	echo "[WARN] Nexus Tools folder found in $HOME/.nexustools. Press ENTER to delete or X to cancel."
 	read -sn1 input
 	[ "$input" = "" ] && rm -rf $HOME/.nexustools
+	echo "[ OK ] Deleted $HOME/.nexustools"
 fi
 
 # Nexus Tools 2.8-3.1 (October 2015-August 2016) installed binaries in /user/local/bin
 if [ -f /usr/local/bin/adb ]; then
 	echo "[WARN] ADB found in /usr/local/bin. It may have been installed there by an old version of Nexus Tools."
-    echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
+	echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
 	read -sn1 input
 	[ "$input" = "" ] && sudo rm /usr/local/bin/adb
+	echo "[ OK ] Deleted /usr/local/bin/adb"
 fi
 if [ -f /usr/local/bin/fastboot ]; then
 	echo "[WARN] Fasboot found in /usr/local/bin. It may have been installed there by an old version of Nexus Tools."
-    echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
+	echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
 	read -sn1 input
 	[ "$input" = "" ] && sudo rm /usr/local/bin/fastboot
+	echo "[ OK ] Deleted /usr/local/bin/fastboot"
 fi
 
 # Nexus Tools 1.0-2.7.1 (December 2013-October 2015) installed binaries in /usr/bin
 if [ -f /usr/bin/adb ]; then
 	echo "[WARN] ADB found in /usr/bin. It may have been installed there by an old version of Nexus Tools."
-    echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
+	echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
 	read -sn1 input
 	[ "$input" = "" ] && sudo rm /usr/bin/adb
+	echo "[ OK ] Deleted /usr/bin/adb"
 fi
 if [ -f /usr/bin/fastboot ]; then
 	echo "[WARN] Fasboot found in /usr/bin. It may have been installed there by an old version of Nexus Tools."
-    echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
+	echo "[WARN] Sudo access is required to remove. Press ENTER to delete or X to cancel."
 	read -sn1 input
 	[ "$input" = "" ] && sudo rm /usr/bin/fastboot
+	echo "[ OK ] Deleted /usr/bin/fastboot"
 fi
 
 # Uninstall Device ID list
