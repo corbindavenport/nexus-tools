fbdaa63f0763fb2f80dacb71a7ecd550be8bb66e
Corbin Davenport <me@corbin.io>
2020-06-12 00:44:30 -0400

    Update version number

--- install.sh
+++ install.sh
@@ -61,14 +61,14 @@ _add_path() {
 		echo "[ OK ] $DIR/ is already in PATH."
 	else
 		# Nexus Tools directory needs to be added to $PATH
-		if [ -f $HOME/.bashrc ]; then
+		if [ -f "$HOME/.bashrc" ]; then
 			echo 'export PATH=$PATH:'$DIR >> $HOME/.bashrc
-			source $HOME/.bashrc
+			source "$HOME/.bashrc"
 			echo "[ OK ] Added $DIR/ to $HOME/.bashrc."
 		fi
-		if [ -f $HOME/.zshrc ]; then
+		if [ -f "$HOME/.zshrc" ]; then
 			echo 'export PATH=$PATH:'$DIR >> $HOME/.zshrc
-			source $HOME/.zshrc
+			source "$HOME/.zshrc"
 			echo "[ OK ] Added $DIR/ to $HOME/.zshrc."
 		fi
 	fi
@@ -107,7 +107,7 @@ _analytics() {
 }
 
 # Start the script
-echo "[INFO] Nexus Tools 4.2"
+echo "[INFO] Nexus Tools 4.2.1"
 
 # Check that required applications are installed
 if ! [ -x "$(command -v curl)" ]; then
