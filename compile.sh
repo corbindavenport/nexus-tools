083599530a9dcda55fbe18a130ab4fbfa793273f
Corbin Davenport <me@corbin.io>
2021-07-24 01:29:44 -0400

    Create compile script

---
+++ compile.sh
@@ -0,0 +1,20 @@
+# This script is for compiling the Dart executable
+# The current path needs to be set to the root of the repository first
+
+OS=$(uname)
+ARCH=$(uname -m)
+FILENAME=''
+
+if [ "$OS" = "Darwin" ]; then
+    # This will need to be updated when M1 support is added
+    FILENAME='nexustools-macos-x64'
+elif [ "$OS" = "Linux" ] && [ "$ARCH" = "x86_64" ]; then
+    FILENAME='nexustools-linux-x64'
+elif [ "$OS" = "Linux" ] && [ "$ARCH" = "amd64" ]; then
+    FILENAME='nexustools-linux-x64'
+else
+    FILENAME='nexustools'
+fi
+
+mkdir -p ./dist
+dart compile exe ./bin/main.dart -o ./dist/$FILENAME
